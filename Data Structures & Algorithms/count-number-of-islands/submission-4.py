fe41012de9ae1a8a00a0f38964346c535dc5f248
Abhishek Saxena <47313080+abhisheksaxenaa@users.noreply.github.com>
2026-04-21 12:20:51 +0530

    Add: count-number-of-islands - submission-4

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-4.py
@@ -0,0 +1,32 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        # TC: O(m * n)
+        # SC: O(m * n)
+        R = len(grid)
+        C = len(grid[0])
+        count = 0
+        # Maintain count with 1, counting number of islands
+        # Iterate through each cell
+        # once '1' is found, change all connected 1s to -1 (a constant value to keep track of visited)
+        # finding connected 1s via dfs?
+        # return (count - 1)
+        def dfs(i, j, R, C):
+            if i >= R or j >= C or i < 0 or j < 0:
+                return
+            if grid[i][j] != '1':
+                return
+            grid[i][j] = '-1'
+            dfs(i + 1, j, R, C)
+            dfs(i, j + 1, R, C)
+            dfs(i - 1, j, R, C)
+            dfs(i, j - 1, R, C)
+
+        for i in range(R):
+            for j in range(C):
+                if grid[i][j] == '1':
+                    # print(f'{i}-{j}')
+                    # put dfs
+                    count += 1
+                    dfs(i, j, R, C)
+        # print(grid)
+        return count
