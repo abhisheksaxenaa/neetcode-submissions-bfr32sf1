b6504971f342b0d7894a2dfb71ac7f5e15953803
Abhishek Saxena <47313080+abhisheksaxenaa@users.noreply.github.com>
2026-08-22 20:41:40 +0530

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,37 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+
+        def matrix_mult(A, B):
+            return [[A[0][0] * B[0][0] + A[0][1] * B[1][0],
+                     A[0][0] * B[0][1] + A[0][1] * B[1][1]],
+                    [A[1][0] * B[0][0] + A[1][1] * B[1][0],
+                     A[1][0] * B[0][1] + A[1][1] * B[1][1]]]
+
+        def matrix_pow(M, p):
+            result = [[1, 0], [0, 1]]
+            base = M
+
+            while p:
+                if p % 2 == 1:
+                    result = matrix_mult(result, base)
+                base = matrix_mult(base, base)
+                p //= 2
+
+            return result
+
+        # M = [[1, 1], [1, 0]]
+        # result = matrix_pow(M, n)
+        # return result[0][0]
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        a = 1
+        b = 2
+        for i in range(2, n):
+            t = b
+            b = b + a
+            a = t
+        return b
