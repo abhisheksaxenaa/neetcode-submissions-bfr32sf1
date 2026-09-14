0fa420d15e42e0cd3010135971e07648aa57d841
Abhishek Saxena <47313080+abhisheksaxenaa@users.noreply.github.com>
2026-09-14 21:29:43 +0530

    Add: word-break - submission-1

---
+++ Data Structures & Algorithms/word-break/submission-1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+
+        # Approach : 1
+        # def dfs(i):
+        #     if i == n:
+        #         return True
+        #     for word in wordDict:
+        #         w = len(word)
+        #         if (i + w) <= n and s[i : i + w] == word:
+        #             if dfs(i + w):
+        #                 return True
+        #     return False
+        # return dfs(0)
+        # n = len(s)
+
+        # Approach 2: using set for wordset, memoization in recursion
+        wordDictSet = set(wordDict)
+        memo = {n: True}
+        def dfs(i):
+            if i in memo:
+                return memo[i]
+            for j in range(i, n):
+                if s[i: j+1] in wordDictSet:
+                    if dfs(j + 1):
+                        memo[i] = True
+                        return True
+            memo[i] = False
+            return False
+        return dfs(0)
